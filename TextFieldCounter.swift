6dbbc884cd7bc13d80b6eb6b09060a53da71c584
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2016-12-07 21:38:04 -0200

    Project

---
+++ TextFieldCounter.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldCounter.swift
+//  TextFieldCounter
+//
+//  Created by Fabricio Serralvo on 12/7/16.
+//  Copyright © 2016 Fabricio Serralvo. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCounter: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
