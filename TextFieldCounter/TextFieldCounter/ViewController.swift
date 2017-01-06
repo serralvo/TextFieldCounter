0e508a3029251a9d238e17d5c26b9dca000a4310
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2017-01-05 22:56:15 -0200

    Visual adjusts and demo image

--- TextFieldCounter/TextFieldCounter/ViewController.swift
+++ TextFieldCounter/TextFieldCounter/ViewController.swift
@@ -16,14 +16,7 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        let frame = CGRect(x: 27, y: 70, width: 320, height: 30)
-        let textField = TextFieldCounter(frame: frame, limit: 20, shouldAnimate: true, colorOfCounterLabel: UIColor.darkGray, colorOfLimitLabel: UIColor.orange)
-        textField.borderStyle = UITextBorderStyle.roundedRect
-        textField.backgroundColor = UIColor.white
-        textField.placeholder = "Type something my friend :)"
-    
-        self.view.addSubview(textField)
+        view.backgroundColor = UIColor(colorLiteralRed: 242/255, green: 242/255, blue: 242/255, alpha: 1)
     }
 
 }
