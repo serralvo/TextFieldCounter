69d0b8ef217fcc635a682c0b7e2d6ef8d69a3fca
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2017-07-01 18:57:29 -0300

    Added some tests

---
+++ TextFieldCounter/TextFieldCounterTests/TextFieldCounterTests.swift
@@ -0,0 +1,38 @@
+//
+//  TextFieldCounterTests.swift
+//  TextFieldCounterTests
+//
+//  Created by Fabricio Serralvo on 7/1/17.
+//  Copyright © 2017 Fabricio Serralvo. All rights reserved.
+//
+
+import XCTest
+@testable import TextFieldCounter
+
+class TextFieldCounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testTextGreaterThanTheLimit() {
+        let textField = TextFieldCounter(frame: CGRect.zero, limit: 10, animate: true, ascending: true, counterColor: .black, limitColor: .red)
+        let text = "hello hello hello"
+        let range = NSMakeRange(0, text.characters.count)
+        
+        XCTAssertFalse(textField.textField(textField, shouldChangeCharactersIn: range, replacementString: text))
+    }
+    
+    func testTextLessThanTheLimit() {
+        let textField = TextFieldCounter(frame: CGRect.zero, limit: 10, animate: true, ascending: true, counterColor: .black, limitColor: .red)
+        let text = "hello"
+        let range = NSMakeRange(0, text.characters.count)
+        
+        XCTAssertTrue(textField.textField(textField, shouldChangeCharactersIn: range, replacementString: text))
+    }
+    
+}
