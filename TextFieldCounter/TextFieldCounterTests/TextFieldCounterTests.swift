29168a60fd9d64ffdf75005c84f19ed991bba9fd
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2019-12-26 20:02:25 -0300

    Updates tests methods of TextFieldCounterTests

--- TextFieldCounter/TextFieldCounterTests/TextFieldCounterTests.swift
+++ TextFieldCounter/TextFieldCounterTests/TextFieldCounterTests.swift
@@ -11,23 +11,27 @@ import XCTest
 
 class TextFieldCounterTests: XCTestCase {
         
-    func test_TextFieldCounter_Text_WhetherGreaterThanTheLimit() {
+    func test_textFieldCounter_whenText_isGreaterThanTheLimit_shouldNotChangeCharacters() {
         let textField = TextFieldCounter(frame: CGRect.zero, limit: 10, animate: true, ascending: true, counterColor: .black, limitColor: .red)
         let text = "hello hello hello"
         let range = NSMakeRange(0, 0)
         
-        XCTAssertFalse(textField.textField(textField, shouldChangeCharactersIn: range, replacementString: text))
+        let shouldChangeCharacters = textField.textField(textField, shouldChangeCharactersIn: range, replacementString: text)
+        
+        XCTAssertFalse(shouldChangeCharacters)
     }
     
-    func test_TextFieldCounter_Text_WhetherLessThanTheLimit() {
+    func test_textFieldCounter_whenText_isLessThanTheLimit_shouldChangeCharacters() {
         let textField = TextFieldCounter(frame: CGRect.zero, limit: 10, animate: true, ascending: true, counterColor: .black, limitColor: .red)
         let text = "hello"
         let range = NSMakeRange(0, 0)
         
-        XCTAssertTrue(textField.textField(textField, shouldChangeCharactersIn: range, replacementString: text))
+        let shouldChangeCharacters = textField.textField(textField, shouldChangeCharactersIn: range, replacementString: text)
+        
+        XCTAssertTrue(shouldChangeCharacters)
     }
     
-    func test_TextFieldCounter_Character_DeletionOfOneCharacter() {
+    func test_textFieldCounter_whenDeleteOneCharacter_counterLabelShouldNotContainDeletedCharacter() {
         let textField = TextFieldCounter(frame: CGRect.zero, limit: 10, animate: true, ascending: true, counterColor: .black, limitColor: .red)
         let initalText = "some text"
         textField.text = initalText
@@ -35,10 +39,11 @@ class TextFieldCounterTests: XCTestCase {
         let deletionText = ""
         let range = NSMakeRange(initalText.count - 2, 1)
         let _ = textField.textField(textField, shouldChangeCharactersIn: range, replacementString: deletionText)
+        
         XCTAssertTrue(textField.counterLabel.text == "8")
     }
     
-    func test_TextFieldCounter_Character_MultipleDeletionOfCharacters() {
+    func test_textFieldCounter_whenDeleteMultipleCharacters_counterLabelShouldNotContainDeletedCharacters() {
         let textField = TextFieldCounter(frame: CGRect.zero, limit: 20, animate: true, ascending: true, counterColor: .black, limitColor: .red)
         let initalText = "The quick brown fox"
         textField.text = initalText
